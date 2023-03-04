61c1e533f4413b9da055966341afa3ce477350fc
Christopher House <chris.house.00@gmail.com>
2023-03-03 21:41:36 -0600

    pdf getting extracted yo (#2)
    Co-authored-by: Chris House <chhouse@microsoft.com>

---
+++ OpenAIPDFSUmmarization/Models/PdfText.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace OpenAIPdfSummarization.Models;
+
+public class PdfText
+{
+    public PdfText()
+    {
+        Pages = new List<string>();
+    }
+
+    public List<string> Pages { get; }
+
+    public string GetPdfText()
+    {
+        var builder = new StringBuilder();
+
+        foreach (var page in Pages)
+        {
+            builder.AppendLine(page);
+        }
+
+        return builder.ToString();
+    }
+
+    public string GetChatPrompt()
+    {
+        return $"Please summarize this text: {GetPdfText()}";
+    }
+}
